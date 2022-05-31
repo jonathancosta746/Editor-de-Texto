25ba7d83ef541ca35cc4f0c947d14c4bd256763a
Jonathan Costa <jonathancosta746@gmail.com>
2022-05-31 11:03:45 -0300

    correção de formatação do código

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
+ var btnBold = document.getElementById('btnBold'),
+    btnItalic = document.getElementById('btnItalic'),
+    btnUnderline = document.getElementById('btnUnderline'),
+    btnColor = document.getElementById('btnColor'),
+    btnBackColor = document.getElementById('btnBlackColor');
 
-    var nome = window.prompt('Olá, você pode escrever textos aqui. Qual seu nome?')
-    
-    
+    btnBold.onclick = function(event){
+        document.execCommand(btnBold.getAttribute("data-type"),false,null);
+    }
         
-        var btnBold = document.getElementById('btnBold'),
-            btnItalic = document.getElementById('btnItalic'),
-            btnUnderline = document.getElementById('btnUnderline'),
-            btnColor = document.getElementById('btnColor'),
-            btnBackColor = document.getElementById('btnBlackColor');
+	btnItalic.onclick = function(event){
+		document.execCommand(btnItalic.getAttribute("data-type"),false,null);
+	}	
 
-        btnBold.onclick = function(event){
-            document.execCommand(btnBold.getAttribute("data-type"),false,null);
-        }
-        
-		btnItalic.onclick = function(event){
-			document.execCommand(btnItalic.getAttribute("data-type"),false,null);
-		}	
+	btnUnderline.onclick = function(event){
+		document.execCommand(btnUnderline.getAttribute("data-type"),false,null);
+	}	
 
-		btnUnderline.onclick = function(event){
-			document.execCommand(btnUnderline.getAttribute("data-type"),false,null);
-		}	
+	btnColor.onclick = function(event){
+		document.execCommand(btnColor.getAttribute("data-type"),false,"#35037F");
+	}	
 
-		btnColor.onclick = function(event){
-			document.execCommand(btnColor.getAttribute("data-type"),false,"#35037F");
-		}	
-
-		btnBackColor.onclick = function(event){
-			document.execCommand(btnBackColor.getAttribute("data-type"),false,"gold");
-        }
+	btnBackColor.onclick = function(event){
+		document.execCommand(btnBackColor.getAttribute("data-type"),false,"gold");
+    }
